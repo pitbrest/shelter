eb6d8af73970fa1914c1a5037e695fbefc967777
pitbrest <sasha.petrov@tut.by>
2022-04-21 10:43:01 +0300

    some commit

--- pages/pets/js/petsSlider.js
+++ pages/pets/js/petsSlider.js
@@ -1,10 +1,5 @@
 const myPetsSlider = new Swiper('.pets-swiper', {
 
-	// Сетка слайдера в 2 строки и построчным заполнением
-	grid: {
-		rows: 2,
-		fill: 'row',
-	},
 	// Скорость прокрутки слайдов
 	speed: 1200,
 	// Включаем/выключаем возможность переключать слайды перетягиванием мыши влево и вправо
@@ -29,13 +24,14 @@ const myPetsSlider = new Swiper('.pets-swiper', {
 		// when window width is >= 320px
 		320: {
 			// Кол-во выводимых слайдов одновременно
-			slidesPerView: 1,			
+			slidesPerView: 1,
 			// Количество одновременно прокручиваемых слайдов за один клик по стрелке
-			slidesPerGroup: 3,
+
 			// Расстояние между карточками
-			spaceBetween: 0,
+			spaceBetween: 40,
 			grid: {
 				rows: 3,
+				column: 1,
 				fill: 'row',
 			},
 		},
@@ -52,6 +48,11 @@ const myPetsSlider = new Swiper('.pets-swiper', {
 			slidesPerView: 4,
 			slidesPerGroup: 4,
 			spaceBetween: 40,
+			// Сетка слайдера в 2 строки и построчным заполнением
+			grid: {
+				rows: 2,
+				fill: 'row',
+			},
 		}
 	},
 });
@@ -82,71 +83,72 @@ toExtremeSlide()
 // Реализуем стилистику кнопок прокрутки слайдов в зависимости 
 // от текущего слайда (согласно заданию)
 
-let controlBtnsStyling = function() {
+let controlBtnsStyling = function () {
 	const paginationBtn = document.querySelector('.swiper-pagination-current');
 	const allSliderBtns = document.querySelectorAll('.content-toggle-button');
-	const stepToLeftBtn =  document.querySelector('.step-left');
-	const stepToRightBtn =  document.querySelector('.step-right');
+	const stepToLeftBtn = document.querySelector('.step-left');
+	const stepToRightBtn = document.querySelector('.step-right');
 	const toLeftSideBtn = document.querySelector('.to-left-side');
 	const toRightSideBtn = document.querySelector('.to-right-side');
 
 	allSliderBtns.forEach(item => {
 		item.addEventListener('click', () => {
-			if(+paginationBtn.innerHTML === 1) {
+
+			if (+paginationBtn.innerHTML === 1) {
 				stepToLeftBtn.classList.remove('active');
 				toLeftSideBtn.classList.remove('active');
-				if(!stepToRightBtn.classList.contains('active')) {
+				if (!stepToRightBtn.classList.contains('active')) {
 					stepToRightBtn.classList.add('active');
-				}; 
+				};
 				if (!toRightSideBtn.classList.contains('active')) {
 					toRightSideBtn.classList.add('active');
 				}
 			}
-			else if(+paginationBtn.innerHTML > 1) {				
-				if(!stepToLeftBtn.classList.contains('active')) {
+			else if (+paginationBtn.innerHTML > 1) {
+				if (!stepToLeftBtn.classList.contains('active')) {
 					stepToLeftBtn.classList.add('active');
-				}; 
+				};
 				if (!toLeftSideBtn.classList.contains('active')) {
 					toLeftSideBtn.classList.add('active');
 				}
 			}
-			if(!(+paginationBtn.innerHTML === 6 && innerWidth >= 1280) && !(+paginationBtn.innerHTML === 8 && innerWidth > 768 && innerWidth < 1280) && !(+paginationBtn.innerHTML === 16 && innerWidth < 768)) {			
-				if(!stepToRightBtn.classList.contains('active')) {
+			if (!(+paginationBtn.innerHTML === 6 && innerWidth >= 1280) && !(+paginationBtn.innerHTML === 8 && innerWidth >= 768 && innerWidth < 1280) && !(+paginationBtn.innerHTML === 16 && innerWidth < 768)) {
+				if (!stepToRightBtn.classList.contains('active')) {
 					stepToRightBtn.classList.add('active');
-				}; 
+				};
 				if (!toRightSideBtn.classList.contains('active')) {
 					toRightSideBtn.classList.add('active');
 				}
 			}
-			else if(+paginationBtn.innerHTML === 6 && innerWidth >= 1280) {
+			else if (+paginationBtn.innerHTML === 6 && innerWidth >= 1280) {
 				stepToRightBtn.classList.remove('active');
 				toRightSideBtn.classList.remove('active');
 				stepToLeftBtn.classList.add('active');
 				toLeftSideBtn.classList.add('active');
 			}
-			else if(+paginationBtn.innerHTML === 8 && innerWidth > 768 && innerWidth < 1280) {
+			if (+paginationBtn.innerHTML === 8 && innerWidth >= 768 && innerWidth < 1280) {
 				stepToRightBtn.classList.remove('active');
 				toRightSideBtn.classList.remove('active');
 				stepToLeftBtn.classList.add('active');
 				toLeftSideBtn.classList.add('active');
 			}
-			else if(+paginationBtn.innerHTML === 16 && innerWidth < 768) {
+			if (+paginationBtn.innerHTML === 16 && innerWidth < 768) {
 				stepToRightBtn.classList.remove('active');
 				toRightSideBtn.classList.remove('active');
 				stepToLeftBtn.classList.add('active');
 				toLeftSideBtn.classList.add('active');
 			}
-			else if(+paginationBtn.innerHTML > 1) {
-				if(!stepToLeftBtn.classList.contains('active')) {
+			else if (+paginationBtn.innerHTML > 1) {
+				if (!stepToLeftBtn.classList.contains('active')) {
 					stepToLeftBtn.classList.add('active');
-				} 
-				if(!toLeftSideBtn.classList.contains('active')) {
+				}
+				if (!toLeftSideBtn.classList.contains('active')) {
 					toLeftSideBtn.classList.add('active');
-				} 
-			}			
+				}
+			}
 		})
-	})	
-	
+	})
+
 }
 controlBtnsStyling()
 
